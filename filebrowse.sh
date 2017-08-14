20f563adbd6e4233073e9448e5c03c5686fc8854
Claude Pageau <pageauc@gmail.com>
2017-08-14 17:27:03 -0400

    Initial Release

---
+++ filebrowse.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+
+: '
+                   FileBrowser written by Claude Pageau
+
+This is a whiptail file browser that allows navigating through a directory
+structure and select a file. It Returns a filename if selected.  
+This sample code can be used in a script menu to perform an operation that
+requires selecting a file.
+
+'
+
+#------------------------------------------------------------------------------
+function Filebrowser()
+{
+# first parameter is Menu Title
+# second parameter is optional dir path to starting folder
+# otherwise current folder is selected
+
+    if [ -z $2 ] ; then
+        dir_list=$(ls -lhp  | awk -F ' ' ' { print $9 " " $5 } ')
+    else
+        cd "$2"
+        dir_list=$(ls -lhp  | awk -F ' ' ' { print $9 " " $5 } ')
+    fi
+
+    curdir=$(pwd)
+    if [ "$curdir" == "/" ] ; then  # Check if you are at root folder
+        selection=$(whiptail --title "$1" \
+                              --menu "PgUp/PgDn/Arrow Enter Selects File/Folder\nor Tab Key\n$curdir" 0 0 0 \
+                              --cancel-button Cancel \
+                              --ok-button Select $dir_list 3>&1 1>&2 2>&3)
+    else   # Not Root Dir so show ../ BACK Selection in Menu
+        selection=$(whiptail --title "$1" \
+                              --menu "PgUp/PgDn/Arrow Enter Selects File/Folder\nor Tab Key\n$curdir" 0 0 0 \
+                              --cancel-button Cancel \
+                              --ok-button Select ../ BACK $dir_list 3>&1 1>&2 2>&3)
+    fi
+
+    RET=$?
+    if [ $RET -eq 1 ]; then  # Check if User Selected Cancel
+       return 1
+    elif [ $RET -eq 0 ]; then
+       if [[ -d "$selection" ]]; then  # Check if Directory Selected
+          Filebrowser "$1" "$selection"
+       elif [[ -f "$selection" ]]; then  # Check if File Selected
+          if [[ $selection == *$filext ]]; then   # Check if selected File has .jpg extension
+            if (whiptail --title "Confirm Selection" --yesno "DirPath : $curdir\nFileName: $selection" 0 0 \
+                         --yes-button "Confirm" \
+                         --no-button "Retry"); then
+                filename="$selection"
+                filepath="$curdir"    # Return full filepath  and filename as selection variables
+            else
+                Filebrowser "$1" "$curdir"
+            fi
+          else   # Not jpg so Inform User and restart
+             whiptail --title "ERROR: File Must have .jpg Extension" \
+                      --msgbox "$selection\nYou Must Select a jpg Image File" 0 0
+             Filebrowser "$1" "$curdir"
+          fi
+       else
+          # Could not detect a file or folder so Try Again
+          whiptail --title "ERROR: Selection Error" \
+                   --msgbox "Error Changing to Path $selection" 0 0
+          Filebrowser "$1" "$curdir"
+       fi
+    fi
+}
+
+menutitle="File Selection Menu"
+startdir="/home/pi"
+filext='jpg'
+
+Filebrowser "$menutitle" "$startdir"
+
+exitstatus=$?
+if [ $exitstatus -eq 0 ]; then
+    if [ "$selection" == "" ]; then
+        echo "User Pressed Esc with No File Selection"
+    else
+        whiptail --title "File was selected" --msgbox " \
+
+        File Selected information
+
+        Filename : $filename
+        Directory: $filepath
+ 
+        \
+        " 0 0 0
+        echo "Perform an operation with this"
+        echo "filepath $filepath/$filename" 
+    fi
+else
+    echo "User Pressed Cancel. with No File Selected"
+fi
+
